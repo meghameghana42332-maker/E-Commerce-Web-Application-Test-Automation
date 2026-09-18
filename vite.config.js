a5a95cb35030289ff328d8be7a13736da064ad73
Meghana <meghameghana42332@gmail.com>
2026-09-18 07:52:53 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative asset paths work correctly when GitHub Pages serves this repo
-  // from /E-Commerce-Web-Application-Test-Automation/
-  base: './'
-})
