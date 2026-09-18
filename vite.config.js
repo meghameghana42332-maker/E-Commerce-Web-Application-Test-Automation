0c42481c57ec74947078b83faabb90f24509d016
Meghana <meghameghana42332@gmail.com>
2026-09-18 07:37:56 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/E-Commerce-Web-Application-Test-Automation/'
+  // Relative asset paths work correctly when GitHub Pages serves this repo
+  // from /E-Commerce-Web-Application-Test-Automation/
+  base: './'
 })
